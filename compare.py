ee96bbfa95aa27b9dd8bf4a73f6e5681b6969397
ibmfarias <ivanfarias.pro@gmail.com>
2025-07-22 15:20:11 -0300

    .

---
+++ compare.py
@@ -0,0 +1,17 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+'''
+teste condicional de forma simplificada com uso de if
+if x < y:
+    print("x in less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+'''
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
