f628eb857854d729ea50a6a52c70404c2d5c10ef
ibmfarias <ivanfarias.pro@gmail.com>
2025-07-25 09:22:26 -0300

    ,

---
+++ meal/meal_v2.py
@@ -0,0 +1,34 @@
+def main():
+    #versão 1
+    #1o passo - Pergunta qual o horário (nesse ponto os valores são armazenados como str)
+    meal_time = input("Please, input a time (e.g. (H:MM), (HH:MM), (H:MM am/pm) or (HH:MM am/pm): ")
+
+    #2o passo - Chama a função convert (é nel que ocorre todo o cálculo)
+    meal_time = convert(meal_time)
+
+    #4o passo - Nesse ponto "meal_time" recebe o valor resultante da função convert, daí aplica-se a condicional e apresenta o resultado
+    if 7.0 <= meal_time <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= meal_time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= meal_time <= 19.0:
+        print("dinner time")
+    else:
+        pass
+
+def convert(time):
+    #3o passo - calcular o horário equivalente a flot e retornar valor para função main()
+    '''
+    "time" recebe o valor da 1o passo (conteúdo da variável meal_time), separa hora e minuto ---> split(":")
+    Armazena os valores em duas variáveis do tipo str (hours_str & minutes_str)
+    Converte os valores para int, transforma para flot como resultado da divisão dos minutos / 60 (ex: 7:30 -> 7.5) e armazena em "time"
+    Retorna "time" para função main
+    '''
+    hours, minutes = map(int, time.split(":"))
+    time = hours + minutes / 60
+    return(time)
+
+if __name__ == "__main__":
+    main()
+
+
